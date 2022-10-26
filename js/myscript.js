98f891cde624bbb2bdb656798cd92975661e8294
unknown <jessehorner11@gmail.com>
2022-10-26 15:32:06 -0600

    so much JS struggling

--- js/myscript.js
+++ js/myscript.js
@@ -4,14 +4,42 @@ const swordArray = [
   "Greatsword",
   "Falchion",
   "Scimitar",
-  "and Rapier.",
+  "Rapier",
 ];
 
 const commaSpace = swordArray.join(", ");
 
-document.getElementById("swordArray").innerHTML = commaSpace;
+let swords = document.getElementById("swordArray");
+
+swords.innerHTML = commaSpace;
+swords.innerHTML = commaSpace.replace("Rapier", "and Rapier");
+
+
+console.log(swordArray.toString().replace("Rapier", "and Rapier"));
+
 
 // For testing
-console.log('My favorite swords are the', commaSpace);
-console.log(swordArray.join(', '))
-console.log(swordArray.join(' + '))
+
+// console.log('My favorite swords are the', commaSpace);
+// console.log(swordList.toString().split())
+// console.log(swordList.join(':').split(':'))
+// console.log(swordList.join(' + '))
+
+// for(i = 0; i < swordList.length; i++){
+//     console.log("Sword" , i, "is", swordList[i]);
+//     if(swordList[i] == "Rapier"){
+//       console.log("and Rapier");
+//     }
+// }
+
+// let word = "swords";
+
+// for(i = 0; i < word.length; i++){
+//     console.log(word[i]);
+// }
+
+// for(x of swordList){
+//   console.log(x);
+// }
+ 
+// I have confused myself.
